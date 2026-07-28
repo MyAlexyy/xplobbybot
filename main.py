4cc063ae4ae185aee9b63ad45200ae5b81f35a30
MyAlexyy <alessiosansica@gmail.com>
2026-07-28 03:55:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ IL_MIO_TAG_URL = "https://t.me/Myalexy"  # Link al tuo profilo Telegram
 
 # Parametri di Connessione MySQL (XAMPP predefinito)
 MYSQL_CONFIG = {
-    "host": "mysql.railway.internal",
-    "user": "root",
-    "password": "rpbuTOnHanNsjljFYFjtHOgKOQCbQfUP",  # Di default in XAMPP la password è vuota
-    "port": 3306,
-    "database": "railway"
+    "host": os.environ.get("MYSQLHOST"),
+    "user": os.environ.get("MYSQLUSER"),
+    "password": os.environ.get("MYSQLPASSWORD"),
+    "port": int(os.environ.get("MYSQLPORT", 3306)),
+    "database": os.environ.get("MYSQLDATABASE", "railway")
 }
 DB_NAME = "railway"
 
